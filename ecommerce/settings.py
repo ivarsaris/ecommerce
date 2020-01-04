c068365891226592f0d8ce39c07cff94cb220001
Ivar Saris <ivarsaris@gmail.com>
2020-01-04 16:01:17 +0000

    create templates and routing

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '7%pu_*-_p(6hgh09d4m81a$mds$b-pqohvx1+k#rfyk@c%g1!!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://8000-f363c095-2a44-4b6a-b67d-fb593bdd0cf4.ws-eu01.gitpod.io/']
+ALLOWED_HOSTS = ['https://8000-f363c095-2a44-4b6a-b67d-fb593bdd0cf4.ws-eu01.gitpod.io/',
+                'localhost']
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailAuth'
+]
